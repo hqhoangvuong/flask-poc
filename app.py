5917ef612c960ea6c859fb0529c480fe90ad2935
Vuong "Hikari" Huynh <hoangvuong19991964@gmail.com>
2023-07-31 12:52:44 +0700

    fix log

--- app.py
+++ app.py
@@ -93,7 +93,7 @@ class Login(Resource):
 class Trigger(Resource):
     @jwt_required
     def get(self, jobstatus):
-        logging.info('Triggering job status {jobstatus} triggered by user {request.user}')
+        logging.info(f"Triggering job status '{jobstatus}' triggered by user '{request.user}'")
         return {'message': f"Job status '{jobstatus}' triggered by user '{request.user}'"}
 
 
